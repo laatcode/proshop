f8574bbb5f652673e9437ab638586ccfddd358db
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2021-07-29 15:35:51 -0500

    Adding backend and serve products

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('API is runing...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find( p => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(port, console.log(`Server running on port ${port}`))
