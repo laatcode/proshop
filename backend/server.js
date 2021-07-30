facba62b8042202169c7f444d377d4f1001d0020
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2021-07-30 11:59:56 -0500

    Implement colors module for color in the console

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -23,4 +24,4 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`))
+app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold))
