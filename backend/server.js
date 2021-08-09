9982a28c5232c0cba8b63880042231a67488b23e
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2021-08-09 16:20:35 -0500

    Products routes and fetching products from de database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -15,13 +15,6 @@ app.get('/', (req, res) => {
     res.send('API is runing...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find( p => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold))
