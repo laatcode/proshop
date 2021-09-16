1c00840a415712b6feb3a3c5f68de0d6bbe97a6d
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2021-09-16 11:53:43 -0500

    Reducers, actions, Screen and functionality for login users

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const FormContainer = ({ children }) => (
+    <Container>
+        <Row className='justify-content-md-center'>
+            <Col xs={12} md={6}>
+                {children}
+            </Col>
+        </Row>
+    </Container>
+)
+
+export default FormContainer
