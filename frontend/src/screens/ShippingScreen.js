3655c195ccc6e3b188bddd74367d1b39aabd6015
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2021-10-13 13:38:46 -0500

    Adding shipping screen and saving address in localstore

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from 'react-redux'
+import Button from "react-bootstrap/esm/Button"
+import Form from 'react-bootstrap/Form'
+import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from "../actions/cartActions"
+import CheckoutSteps from "../components/CheckoutSteps"
+
+const ShippingScreen = ({ history }) => {
+
+    const cart = useSelector(state => state.cart)
+    const { shippingAddress } = cart
+
+    const [address, setAddress] = useState(shippingAddress.address)
+    const [city, setCity] = useState(shippingAddress.city)
+    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+    const [country, setCountry] = useState(shippingAddress.country)
+
+    const dispatch = useDispatch()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(saveShippingAddress({ address, city, postalCode, country }))
+        history.push('/payment')
+    }
+
+    return <FormContainer>
+        <CheckoutSteps step1 step2 />
+        <h1>Shipping</h1>
+        <Form onSubmit={e => submitHandler(e)}>
+            <Form.Group controlId='address' className='mb-3'>
+                <Form.Label>Address</Form.Label>
+                <Form.Control type='text' placeholder='Enter address' value={address} required onChange={(e) => setAddress(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group controlId='city' className='mb-3'>
+            <Form.Label>City</Form.Label>
+                <Form.Control type='text' placeholder='Enter city' value={city} required onChange={(e) => setCity(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Group controlId='postalCode' className='mb-3'>
+                <Form.Control type='text' placeholder='Enter postal code' value={postalCode} required onChange={(e) => setPostalCode(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group controlId='country' className='mb-3'>
+                <Form.Label>Country</Form.Label>
+                <Form.Control type='text' placeholder='Enter country' value={country} required onChange={(e) => setCountry(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+                Continue
+            </Button>
+        </Form>
+    </FormContainer>
+}
+
+export default ShippingScreen
