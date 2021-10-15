d5b67cee7b7b8eed52bbd40652ed196d5ec324ae
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2021-10-15 11:59:47 -0500

    Adding Endpoint to get a single Order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const userRoutes = express.Router()
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 userRoutes.route('/').post(protect, addOrderItems)
+userRoutes.route('/:id').get(protect, getOrderById)
 
 export default userRoutes
